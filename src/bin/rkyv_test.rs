1f42803c5697710aba2ff7980787e16268b62051
Bugen Zhao <i@bugenzhao.com>
2025-08-20 18:44:41 +0700

    serialize ref instead
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- src/bin/rkyv_test.rs
+++ src/bin/rkyv_test.rs
@@ -1,10 +1,74 @@
+use std::sync::Arc;
+
 use rkyv::{
-    munge::munge, rancor::Error, seal::Seal, vec::ArchivedVec, Archive, ArchiveUnsized,
-    Deserialize, Serialize,
+    munge::munge,
+    rancor::Error,
+    seal::Seal,
+    string::ArchivedString,
+    vec::ArchivedVec,
+    with::{AsString, AsVec, Inline, InlineAsBox},
+    Archive, Serialize,
 };
 
-#[derive(Clone, Debug, Serialize, Deserialize, Archive)]
+use crate::mmm::ArchivedRow;
+
+#[derive(Clone, Debug)]
+struct SpanString {
+    data: Arc<str>,
+    start: usize,
+    end: usize,
+}
+
+impl SpanString {
+    fn as_str(&self) -> &str {
+        &self.data[self.start..self.end]
+    }
+}
+
+// mod span_string_archive_with {
+//     use rkyv::{
+//         rancor::{Fallible, Source},
+//         string::{ArchivedString, StringResolver},
+//         with::{ArchiveWith, AsString, SerializeWith},
+//         SerializeUnsized,
+//     };
+
+//     use crate::ArchivedDatum;
+
+//     use super::SpanString;
+
+//     impl ArchiveWith<SpanString> for AsString {
+//         type Archived = ArchivedString;
+
+//         type Resolver = StringResolver;
+
+//         fn resolve_with(
+//             field: &SpanString,
+//             resolver: Self::Resolver,
+//             out: rkyv::Place<Self::Archived>,
+//         ) {
+//             ArchivedString::resolve_from_str(field.as_str(), resolver, out);
+//         }
+//     }
+
+//     impl<S> SerializeWith<SpanString, S> for AsString
+//     where
+//         str: SerializeUnsized<S>,
+//         S: Fallible + ?Sized,
+//         S::Error: Source,
+//     {
+//         fn serialize_with(
+//             field: &SpanString,
+//             serializer: &mut S,
+//         ) -> Result<Self::Resolver, <S as Fallible>::Error> {
+//             ArchivedString::serialize_from_str(field.as_str(), serializer)
+//         }
+//     }
+// }
+
+#[derive(Clone, Debug)]
 enum Datum {
+    // SpanString(#[rkyv(with = AsString)] SpanString),
     String(String),
     Bytes(Vec<u8>),
 }
@@ -12,25 +76,28 @@ enum Datum {
 impl Datum {
     fn to_ref(&self) -> DatumRef {
         match self {
+            // Datum::SpanString(s) => DatumRef::String(s.as_str()),
             Datum::String(s) => DatumRef::String(s.as_str()),
             Datum::Bytes(a) => DatumRef::Bytes(a.as_slice()),
         }
     }
 }
 
-impl ArchivedDatum {
-    fn to_ref(&self) -> DatumRef {
-        match self {
-            ArchivedDatum::String(s) => DatumRef::String(s.as_str()),
-            ArchivedDatum::Bytes(a) => DatumRef::Bytes(a.as_slice()),
-        }
-    }
+#[derive(Clone, Debug, Serialize, Archive)]
+enum DatumRef<'a> {
+    String(#[rkyv(with = AsString)] &'a str),
+    Bytes(#[rkyv(with = AsVec)] &'a [u8]),
 }
 
-#[derive(Clone, Debug)]
-enum DatumRef<'a> {
-    String(&'a str),
-    Bytes(&'a [u8]),
+type ArchivedDatum = ArchivedDatumRef<'static>;
+
+impl ArchivedDatumRef<'_> {
+    fn to_ref(&self) -> DatumRef<'_> {
+        match self {
+            ArchivedDatumRef::String(s) => DatumRef::String(s.as_str()),
+            ArchivedDatumRef::Bytes(b) => DatumRef::Bytes(&*b),
+        }
+    }
 }
 
 #[auto_impl::auto_impl(&)]
@@ -42,23 +109,13 @@ trait Row {
     fn len(&self) -> usize;
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize, Archive)]
+#[derive(Clone, Debug)]
 pub struct VecDatum(Vec<Datum>);
 
 impl Row for VecDatum {
     fn at(&self, index: usize) -> DatumRef {
         self.0[index].to_ref()
     }
-    fn len(&self) -> usize {
-        self.0.len()
-    }
-}
-
-impl Row for ArchivedVecDatum {
-    fn at(&self, index: usize) -> DatumRef {
-        self.0[index].to_ref()
-    }
-
     fn len(&self) -> usize {
         self.0.len()
     }
@@ -75,16 +132,68 @@ impl<'a> Row for VecDatumRef<'a> {
     }
 }
 
+mod mmm {
+    use rkyv::{
+        rancor::{Fallible, Source},
+        vec::VecResolver,
+        with::{ArchiveWith, SerializeWith},
+        SerializeUnsized,
+    };
+
+    use super::*;
+
+    pub(crate) struct MyWith;
+
+    impl<R: Row> ArchiveWith<R> for MyWith {
+        type Archived = ArchivedVec<ArchivedDatum>;
+
+        type Resolver = VecResolver;
+
+        fn resolve_with(row: &R, resolver: Self::Resolver, out: rkyv::Place<Self::Archived>) {
+            ArchivedVec::resolve_from_len(row.len(), resolver, out);
+        }
+    }
+
+    impl<R: Row, S> SerializeWith<R, S> for MyWith
+    where
+        S: Fallible + ?Sized + rkyv::ser::Allocator + rkyv::ser::Writer,
+        S::Error: Source,
+    {
+        fn serialize_with(
+            row: &R,
+            serializer: &mut S,
+        ) -> Result<Self::Resolver, <S as Fallible>::Error> {
+            ArchivedVec::serialize_from_iter((0..row.len()).map(|i| row.at(i)), serializer)
+        }
+    }
+
+    // TODO: new type
+    pub type ArchivedRow = ArchivedVec<ArchivedDatum>;
+
+    impl Row for ArchivedRow {
+        fn at(&self, index: usize) -> DatumRef {
+            self[index].to_ref()
+        }
+
+        fn len(&self) -> usize {
+            self.len()
+        }
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Archive)]
+struct RowHelper<R: Row>(#[rkyv(with = mmm::MyWith)] R);
+
 fn main() {
     let row = VecDatum(vec![
         Datum::String("hello".to_owned()),
         Datum::Bytes(b"world".to_vec()),
     ]);
 
-    let srow = rkyv::to_bytes::<Error>(&row).unwrap();
+    let srow = rkyv::to_bytes::<Error>(&RowHelper(&row)).unwrap();
 
     // print!("{:x?}", srow);
-    let drow = unsafe { rkyv::access_unchecked::<ArchivedVecDatum>(&srow) };
+    let drow = rkyv::access::<ArchivedRow, Error>(&srow).unwrap_or_else(|e| panic!("{e}"));
 
     println!("{:?}", (&drow).at(0));
     println!("{:?}", (&drow).at(1));
